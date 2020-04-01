7f686138c4ed1fac47cc10bbe50170b8373afb6e
Christopher Roman <chrisjiroman@gmail.com>
2020-04-01 13:31:12 -0400

    shared js files, and additional components files

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,60 +1,63 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
+function RenderCampsite({ campsite }) {
+    return (
+        <div key={campsite.id} className="col-md-5 m-1">
+            <Card>
+                <CardImg top width="100%" src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
 
-class CampsiteInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedCampsite: null
-        };
-    };
-
-    renderCampsite(campsite) {
+function RenderComments({ comments }) {
+    if (comments) {
         return (
-            <div key={campsite.id} className="col-md-5 m-1">
-                <Card>
-                    <CardImg top width="100%" src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
+            <div className="col-md-5 m-1">
+                <h4>Comments</h4>
+                {comments.map(comment => {
+                    return (
+                        <div key={comment.id}>
+                            <p>{comment.text} <br />
+                            --{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+                            </p>
+                        </div>
+                    );
+                })}
             </div>
         );
     }
-
-    renderComments(campsite) {
-        if (this.props.campsite.comments) {
-            return (
-                <div className="col-md-5 m-1">
-                    <h4>comments</h4>
-                    {
-                        this.props.campsite.comments.map(comment => <div key={comment.id}><p>{comment.text} <br />--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p></div>)
-                    }
-                </div>
-            )
-        }
-    }
-
-    render() {
-        if (this.props.campsite) {
-            return (
-                <div className="container">
-                    <div className="row">
-                        {(this.renderCampsite(this.props.campsite))}
-                        {(this.renderComments(this.props.campsite.comments))}
-                    </div>
-                </div>
-            )
-
-
-        };
-
-        return (
-            <div />
-        )
-    }
+    return <div />
 }
 
-export default CampsiteInfo;
+function CampsiteInfo(props) {
+            if (props.campsite) {
+                return (
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <Breadcrumb>
+                                    <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                                    <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+                                </Breadcrumb>
+                                <h2>{props.campsite.name}</h2>
+                                <hr />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <RenderCampsite campsite={props.campsite} />
+                            <RenderComments comments={props.comments} />
+                        </div>
+                    </div>
+                );
+            }
+            return <div />;
+        }
+
+
+        export default CampsiteInfo;
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 class Directory extends Component {
@@ -9,17 +10,29 @@ class Directory extends Component {
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
                     <Card>
+                        <Link to={`/directory/${campsite.id}`}>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>
                         </CardImgOverlay>
-                    </Card>
+                        </Link>
+                    </Card>   
                 </div>
             );
         });
 
         return (
             <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>Directory</BreadcrumbItem>
+                        </Breadcrumb>
+                        <h2>Directory</h2>
+                        <hr />
+                    </div>
+                </div>
                 <div className="row">
                     {directory}
                 </div>
